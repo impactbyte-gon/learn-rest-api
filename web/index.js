8ec495ac7572faf4ef931244b42f90b67e2cffce
M Haidar Hanif <git@mhaidarhanif.com>
2019-02-18 16:48:46 +0700

    Render data into HTML

--- web/index.js
+++ web/index.js
@@ -1,4 +1,19 @@
 axios
   .get('https://api.github.com/users/mhaidarh')
-  .then(response => console.log(response.data))
+  .then(response => {
+    const $data = document.getElementById('data')
+    const $userUsername = document.getElementById('user-username')
+    const $userAvatar = document.getElementById('user-avatar')
+    const $userURL = document.getElementById('user-url')
+
+    const userUsername = response.data.login
+    const userAvatar = response.data.avatar_url
+    const userURL = response.data.html_url
+
+    $userUsername.innerHTML = userUsername
+    $userAvatar.setAttribute('src', userAvatar)
+    $userURL.setAttribute('href', userURL)
+    $userURL.innerHTML = userURL
+    $data.innerHTML = JSON.stringify(response.data, null, 2)
+  })
   .catch(error => console.log(error))
